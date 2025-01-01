2faf0d445c4288f1b9744d51071c11ecfa64a5f1
Simon Asika <asika32764@gmail.com>
2025-01-01 15:10:45 +0800

    Add microdata #1

--- src/Module/Front/Faq/views/faq-list.blade.php
+++ src/Module/Front/Faq/views/faq-list.blade.php
@@ -1,5 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
+namespace App\View;
+
 /**
  * Global variables
  * --------------------------------------------------------------
@@ -12,8 +16,7 @@
  * @var $lang      LangService     The language translation service.
  */
 
-declare(strict_types=1);
-
+use Lyrasoft\Faq\Entity\Faq;
 use Windwalker\Core\Application\AppContext;
 use Windwalker\Core\Asset\AssetService;
 use Windwalker\Core\DateTime\ChronosService;
@@ -21,31 +24,42 @@ use Windwalker\Core\Language\LangService;
 use Windwalker\Core\Router\Navigator;
 use Windwalker\Core\Router\SystemUri;
 
+/**
+ * @var $item Faq
+ */
 ?>
 
 @extends('global.body')
 
 @section('content')
-    <div class="l-faq-list">
+    <div class="l-faq-list" itemscope itemtype="https://schema.org/FAQPage">
         <form action="{{ $nav->to('faq_list') }}" method="GET" enctype="multipart/form-data">
             <div class="container">
                 <div class="d-flex justify-content-end">
                     <div class="input-group">
-                        <input type="text" name="search[*]" value="{{ $search['*'] }}" class="form-control" placeholder="請輸入關鍵字" />
+                        <input type="text" name="search[*]" value="{{ $search['*'] }}" class="form-control"
+                            placeholder="請輸入關鍵字" />
                     </div>
                 </div>
                 <div class="accordion my-4" id="faq-accordion">
                     @foreach($items as $i => $item)
-                        <div class="accordion-item">
-                            <div class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-collapse-{{ $i }}">
-                                    {{ $item->title }}
+                        <div class="accordion-item"
+                            itemscope itemprop="mainEntity"
+                            itemtype="https://schema.org/Question"
+                        >
+                            <div class="accordion-header" itemprop="name">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#faq-collapse-{{ $i }}">
+                                    {{ $item->getTitle() }}
                                 </button>
                             </div>
 
-                            <div id="faq-collapse-{{ $i }}" class="accordion-collapse collapse" data-bs-parent="#faq-accordion">
-                                <div class="accordion-body">
-                                    {!! $item->description !!}
+                            <div id="faq-collapse-{{ $i }}" class="accordion-collapse collapse"
+                                data-bs-parent="#faq-accordion">
+                                <div class="accordion-body"
+                                    itemscope itemprop="acceptedAnswer"
+                                    itemtype="https://schema.org/Answer">
+                                    {!! $item->getDescription() !!}
                                 </div>
                             </div>
                         </div>
